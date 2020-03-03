f512ab01126b2414376beb14ed0397c03c833cca
Gözde ŞAHİN YILMAZ <sahin_gozde@yahoo.com>
2020-03-03 12:57:58 +0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { warningAlert } from './warningAlert/warningAlert.component';
+import { successAlert } from './successAlert/successAlert.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    warningAlert,
+    successAlert,
+    AlertComponent
   ],
   imports: [
     BrowserModule
